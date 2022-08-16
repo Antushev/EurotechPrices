f089ebac862831adba6fd5b33a3abccf1050c831
Dmitry Antushev <fyneitd@mail.ru>
2022-08-16 13:51:05 +0300

    Add new layout blocks

--- source/js/script.js
+++ source/js/script.js
@@ -1,7 +1,25 @@
 let pricePopupElement = document.querySelector('.popup-price');
 let buttonClosePricePopupElement = document.querySelector('.popup-price__button-close');
+let trElementHeader = document.querySelector('.table__tr--header');
+let editFirmsView = document.querySelector('.table-price__filter-header');
 
-console.log(pricePopupElement);
+trElementHeader.addEventListener('mouseover', () => {
+  console.log('Наведение на tr');
+
+  editFirmsView.classList.remove('visually-hidden');
+});
+
+trElementHeader.addEventListener('mouseout', () => {
+  editFirmsView.classList.add('visually-hidden');
+});
+
+editFirmsView.addEventListener('mouseover', () => {
+  editFirmsView.classList.remove('visually-hidden');
+});
+
+editFirmsView.addEventListener('mouseout', () => {
+  editFirmsView.classList.add('visually-hidden');
+});
 
 buttonClosePricePopupElement.addEventListener('click', (evt) => {
   evt.preventDefault();
